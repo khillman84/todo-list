cf053692c5bc2742b8db509d2f169be3245a1ccd
Kyle Hillman <khillman84@gmail.com>
2017-05-08 14:17:02 -0700

    worked through class

---
+++ todo-list/todo-list/ViewController.m
@@ -0,0 +1,114 @@
+//
+//  ViewController.m
+//  todo-list
+//
+//  Created by Kyle Hillman on 5/8/17.
+//  Copyright © 2017 Kyle Hillman. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "LoginViewController.h"
+
+
+@import FirebaseAuth;
+@import Firebase;
+
+
+@interface ViewController ()
+
+@property(strong, nonatomic) FIRDatabaseReference *userReference;
+@property(strong, nonatomic) FIRUser *currentUser;
+@property(nonatomic) FIRDatabaseHandle allTodosHandler;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self checkUserStatus];
+}
+
+-(void)checkUserStatus {
+    
+    if (![[FIRAuth auth] currentUser]) {
+        LoginViewController *loginController = [self.storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+        
+        [self presentViewController:loginController animated:YES completion:nil];
+    } else {
+        [self setupFirebase];
+        [self startMonitoringTodoUpdates];
+    }
+}
+
+-(void)setupFirebase {
+    
+    FIRDatabaseReference *databaseReference = [[FIRDatabase database] reference];
+    self.currentUser = [[FIRAuth auth] currentUser];
+    
+    self.userReference = [[databaseReference child:@"users"] child:self.currentUser.uid];
+    
+    NSLog(@"User Reference: %@", self.userReference);
+}
+
+-(void)startMonitoringTodoUpdates {
+    
+    self.allTodosHandler = [[self.userReference child:@"todos"] observeEventType:FIRDataEventTypeValue withBlock:^(FIRDataSnapshot * _Nonnull snapshot) {
+        NSMutableArray *allTodos = [[NSMutableArray alloc]init];
+        
+        for (FIRDataSnapshot *child in snapshot.children) {
+            NSDictionary *todoData = child.value;
+            NSString *todoTitle = todoData[@"title"];
+            NSString *todoContent = todoData[@"content"];
+                
+            //for lab append new todo to all todos array
+                NSLog(@"Todo Title: %@ - Content: %@", todoTitle, todoContent);
+        }
+    }];
+}
+
+@end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
