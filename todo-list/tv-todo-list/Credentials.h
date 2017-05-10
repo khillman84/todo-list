418b9f95a892696aa38421881715504297f2603b
Kyle Hillman <khillman84@gmail.com>
2017-05-10 12:07:44 -0700

    worked through class

---
+++ todo-list/tv-todo-list/Credentials.h
@@ -0,0 +1,15 @@
+//
+//  Credentials.h
+//  todo-list
+//
+//  Created by Kyle Hillman on 5/10/17.
+//  Copyright © 2017 Kyle Hillman. All rights reserved.
+//
+
+#ifndef Credentials_h
+#define Credentials_h
+
+#define APP_KEY @"e7oJFlQnoHS7JMH14yHSvAvCNB23IhNIoubAE2CT"
+
+
+#endif /* Credentials_h */
